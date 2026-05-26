a2514bacd1cbd746a1773f007e396e5cb36f0110
guilhermeymarques-ctrl <guilherme.y.marques@edu.senai.br>
2026-05-26 11:25:53 -0300

    Update server.py

--- server.py
+++ server.py
@@ -37,6 +37,6 @@ def get_leads():
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
     server.py
